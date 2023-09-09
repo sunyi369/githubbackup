9b72c036e53600b6e799a90fd9c4f0d0ae8d1113
sunyi369 <25546385+sunyi369@users.noreply.github.com>
2023-09-09 23:31:49 +0800

    Update download.sh
    重构

--- download.sh
+++ download.sh
@@ -1,70 +1,183 @@
 #!/bin/bash
-#要下载的项目，不够自己加
-repo_names=("aria2/aria2" "aria2/aria2")
-#下载路径
-download_path="/root"
-echo 下载路径为$download_path
-mkdir -p $download_path
-echo 建立下载目录$download_path
-#定义代理判断代理是否能用，自己改
-proxy="192.168.1.255:7890"
-curl --connect-timeout 3 --socks5 $proxy http://www.google.com/ &> /dev/null
-if [ $? -eq 0 ]
-then
- echo "$(date) - 代理ok"
- echo "$(date) - 代理ok" >> $download_path/update.log
- export http_proxy=socks5://$proxy
- export https_proxy=socks5://$proxy
-else
-  echo "$(date) - 代理歇菜,不设置代理"
-  echo "$(date) - 代理歇菜,不设置代理" >> $download_path/update.log
- export http_proxy=""
- export https_proxy=""
+
+# 设置代理服务器
+PROXY_ADDRESS="socks5://192.168.1.1:7890"  # 将地址和端口替换为你的 SOCKS5 代理信息
+
+# GitHub个人访问令牌
+GITHUB_TOKEN="your token"
+
+# 指定GitHub API地址
+GITHUB_API="https://api.github.com/repos/"
+
+# 指定下载根目录
+DOWNLOAD_ROOT_DIR="/root/download"
+
+# 定义要处理的GitHub仓库名称和版本号
+repo_names=("ventoy/Ventoy" "XIU2/CloudflareSpeedTest")
+
+# 设置日志文件路径
+LOG_DIR="$DOWNLOAD_ROOT_DIR/logs"
+LOG_FILE="$LOG_DIR/script.log"
+
+# 检查日志文件所在的目录是否存在，如果不存在则创建
+if [ ! -d "$LOG_DIR" ]; then
+    mkdir -p "$LOG_DIR"
 fi
 
-for repo_name in ${repo_names[@]}; do
-  echo 当前下载项目为$repo_name
-  # 获取最新版本的发布信息
-  release_info=$(curl -s https://api.github.com/repos/$repo_name/releases/latest)
-  current_version=$(echo "$release_info" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-  name1="${repo_name%/*}"
-  name2="${repo_name#*/}"
-  echo  获取项目版本为$current_version
+# 设置日志文件
+exec > >(tee -a "$LOG_FILE") 2>&1
 
-  version_file="$download_path/$name1-$name2/$name2-ver.txt"
-  dir_name="$download_path/$name1-$name2/$name2-ver$current_version/"
-  echo  创建文件夹$dir_name
-  mkdir -p $dir_name
-  echo "查找$name2更新"
-  touch $version_file
-  if [ -f $version_file ]; then
-      stored_version=$(cat $version_file)
-      if [ "$current_version" == "$stored_version" ]; then
-          echo "$(date) - $name2 没有更新" >> $download_path/update.log
-          echo "$(date) - $name2 没有更新"
-           continue
-      fi
-  fi
-  
+# 检查代理服务器是否可用，并将结果写入日志
+if [ -n "$PROXY_ADDRESS" ]; then
+    if ! timeout 5 curl -I -x "$PROXY_ADDRESS" -s "https://google.com" &>/dev/null; then
+        echo "代理服务器不可用，将不使用代理连接" | tee -a "$LOG_FILE"
+        PROXY_ADDRESS=""
+    else
+        echo "代理服务器可用" | tee -a "$LOG_FILE"
+    fi
+fi
 
-  # 从发布信息中提取更新日志
-  update_log=$(echo "$release_info" | sed -n 's/.*"body": "\(.*\)".*/\1/p')
-  # 打印更新日志
-  echo "$name2-ver$current_version$update_log" >> $dir_name/updatedetail.log
-  echo "$name2-ver$current_version$update_log" >> $download_path/Allupdatedetail.log
+# 检查网络连接是否可用，并将结果写入日志
+if ! timeout 5 ping -c 1 baidu.com &>/dev/null; then
+    echo "网络连接不可用" | tee -a "$LOG_FILE"
+    exit 1
+else
+    echo "网络连接可用" | tee -a "$LOG_FILE"
+fi
 
-  assets_url=$(echo "$release_info" | grep '"assets_url"' | sed -E 's/.*"assets_url": "([^"]+)".*/\1/')
-  echo 最新版本的资源集合为$assets_url
+# 函数定义：下载文件并支持重试
+download_with_retry() {
+    local url="$1"
+    local output_file="$2"
+    local max_retries=3
+    local retry_count=0
 
-  for asset_url in $(curl -s $assets_url | grep '"browser_download_url":' | sed -E 's/.*"browser_download_url": "([^"]+)".*/\1/'); do
-  echo $(basename $asset_url)下载地址为$asset_url
-  echo "文件下载到 $dir_name$(basename $asset_url)"
-  #curl -sLJO $asset_url -o "$download_path/$name1/$name1-$current_version/"
-  #wget -P "$download_path/$name1/$name1-$current_version/" $asset_url
-  cd $dir_name && { curl -JLO --progress-bar $asset_url ; cd -; }
-  done
+    until [ $retry_count -ge $max_retries ]; do
+        if timeout 300 curl -L -o "$output_file" -x "$PROXY_ADDRESS" "$url"; then
+            echo "下载文件成功: $output_file" | tee -a "$LOG_FILE"
+            break
+        else
+            retry_count=$((retry_count + 1))
+            echo "下载文件失败，正在重试 (次数: $retry_count)..." | tee -a "$LOG_FILE"
+        fi
+    done
 
-  echo 更新$name2的版本号为$current_version
-  echo $current_version > $version_file
-  echo "$(date) - $name2 更新到版本 $current_version" >> $download_path/update.log
+    if [ $retry_count -eq $max_retries ]; then
+        echo "下载文件失败达到最大重试次数，放弃: $output_file" | tee -a "$LOG_FILE"
+    fi
+}
+
+# 循环遍历GitHub仓库列表
+for repo_name in "${repo_names[@]}"; do
+    # 获取GitHub仓库的最新版本号
+    latest_version_release=$(timeout 300 curl -s -H "Authorization: token $GITHUB_TOKEN" "$GITHUB_API$repo_name/releases/latest" | jq -r ".tag_name")
+    latest_version_prerelease=$(timeout 300 curl -s -H "Authorization: token $GITHUB_TOKEN" "$GITHUB_API$repo_name/releases" | jq -r '.[] | select(.prerelease == true) | .tag_name' | head -n 1)
+    
+    # 将仓库名中的斜杠替换为破折号，用于文件夹命名
+    repo_folder_name="${repo_name/\//-}"
+
+    # 创建下载目录（如果不存在）
+    mkdir -p "$DOWNLOAD_ROOT_DIR/$repo_folder_name"
+
+    # 创建下载目录，包括仓库名和版本号
+    download_dir_release="$DOWNLOAD_ROOT_DIR/$repo_folder_name/$latest_version_release"
+    download_dir_prerelease="$DOWNLOAD_ROOT_DIR/$repo_folder_name/$latest_version_prerelease"
+    
+    # 检查本地是否存在相同版本的 release 项目，如果存在，则跳过下载
+    if [ -d "$download_dir_release" ]; then
+        echo "项目 '$repo_folder_name' 版本 '$latest_version_release' 已存在于本地，跳过下载 release 版本" | tee -a "$LOG_FILE"
+    else
+        mkdir -p "$download_dir_release"
+
+        # 获取最新版本的 release Source code 下载链接
+        tarball_url_release=$(timeout 300 curl -s -H "Authorization: token $GITHUB_TOKEN" "$GITHUB_API$repo_name/releases/latest" | jq -r ".tarball_url")
+        
+        if [ -z "$tarball_url_release" ]; then
+            echo "无法获取GitHub仓库 '$repo_name' 的最新 release 版本下载链接" | tee -a "$LOG_FILE"
+        else
+            # 下载 release Source code 文件
+            download_with_retry "$tarball_url_release" "$download_dir_release/source_code.tar.gz"
+
+            # 下载 release Release 文件
+            release_assets_release=$(timeout 300 curl -s -H "Authorization: token $GITHUB_TOKEN" "$GITHUB_API$repo_name/releases/latest" | jq -r ".assets[] | .name, .browser_download_url")
+
+            if [ -z "$release_assets_release" ]; then
+                echo "无法获取GitHub仓库 '$repo_name' 最新 release 版本的 Release 文件列表" | tee -a "$LOG_FILE"
+            else
+                IFS=$'\n'
+                asset_arr_release=($release_assets_release)
+                unset IFS
+
+                for ((i = 0; i < ${#asset_arr_release[@]}; i += 2)); do
+                    asset_name_release="${asset_arr_release[$i]}"
+                    asset_url_release="${asset_arr_release[$i + 1]}"
+
+                    download_with_retry "$asset_url_release" "$download_dir_release/$asset_name_release"
+                done
+            fi
+        fi
+    fi
+    
+    # 检查本地是否存在相同版本的 prerelease 项目，如果存在，则跳过下载
+    if [ -d "$download_dir_prerelease" ]; then
+        echo "项目 '$repo_folder_name' 版本 '$latest_version_prerelease' 已存在于本地，跳过下载 prerelease 版本" | tee -a "$LOG_FILE"
+    else
+        mkdir -p "$download_dir_prerelease"
+
+        # 获取最新版本的 prerelease Source code 下载链接
+        tarball_url_prerelease=$(timeout 300 curl -s -H "Authorization: token $GITHUB_TOKEN" "$GITHUB_API$repo_name/releases/tags/$latest_version_prerelease" | jq -r ".tarball_url")
+        
+        if [ -z "$tarball_url_prerelease" ]; then
+            echo "无法获取GitHub仓库 '$repo_name' 的最新 prerelease 版本下载链接" | tee -a "$LOG_FILE"
+        else
+            # 下载 prerelease Source code 文件
+            download_with_retry "$tarball_url_prerelease" "$download_dir_prerelease/source_code.tar.gz"
+
+            # 下载 prerelease Release 文件
+            release_assets_prerelease=$(timeout 300 curl -s -H "Authorization: token $GITHUB_TOKEN" "$GITHUB_API$repo_name/releases/tags/$latest_version_prerelease" | jq -r ".assets[] | .name, .browser_download_url")
+
+            if [ -z "$release_assets_prerelease" ]; then
+                echo "无法获取GitHub仓库 '$repo_name' 最新 prerelease 版本的 Release 文件列表" | tee -a "$LOG_FILE"
+            else
+                IFS=$'\n'
+                asset_arr_prerelease=($release_assets_prerelease)
+                unset IFS
+
+                for ((i = 0; i < ${#asset_arr_prerelease[@]}; i += 2)); do
+                    asset_name_prerelease="${asset_arr_release[$i]}"
+                    asset_url_prerelease="${asset_arr_release[$i + 1]}"
+
+                    download_with_retry "$asset_url_prerelease" "$download_dir_prerelease/$asset_name_prerelease"
+                done
+            fi
+        fi
+    fi
+
+    # 获取并存储当前版本的更新日志
+    release_notes_url_release="$GITHUB_API$repo_name/releases/latest"
+    release_notes_url_prerelease="$GITHUB_API$repo_name/releases/tags/$latest_version_prerelease"
+    
+    release_notes_file_release="$download_dir_release/release_notes.txt"
+    release_notes_file_prerelease="$download_dir_prerelease/release_notes.txt"
+
+    if [ -n "$GITHUB_TOKEN" ]; then
+        # 如果有GitHub Token，可以获取更新日志
+        release_notes_release=$(timeout 300 curl -s -H "Authorization: token $GITHUB_TOKEN" "$release_notes_url_release" | jq -r ".body")
+        release_notes_prerelease=$(timeout 300 curl -s -H "Authorization: token $GITHUB_TOKEN" "$release_notes_url_prerelease" | jq -r ".body")
+
+        # 检查更新日志文件是否已存在，如果不存在才写入文件
+        if [ ! -f "$release_notes_file_release" ]; then
+            echo "$release_notes_release" > "$release_notes_file_release"
+            echo "保存项目 '$repo_folder_name' 最新 release 版本 '$latest_version_release' 的更新日志到 '$release_notes_file_release'" | tee -a "$LOG_FILE"
+        fi
+
+        if [ ! -f "$release_notes_file_prerelease" ]; then
+            echo "$release_notes_prerelease" > "$release_notes_file_prerelease"
+            echo "保存项目 '$repo_folder_name' 最新 prerelease 版本 '$latest_version_prerelease' 的更新日志到 '$release_notes_file_prerelease'" | tee -a "$LOG_FILE"
+        fi
+    else
+        echo "未提供GitHub Token，无法获取更新日志" | tee -a "$LOG_FILE"
+    fi
 done
+
+echo "脚本执行完成" | tee -a "$LOG_FILE"
